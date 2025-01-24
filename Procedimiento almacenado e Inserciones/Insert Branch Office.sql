4c0caed9195ff5080a89dd8cfac54a4e64887bd9
Sergio Alexander Garcia Rodriguez <sgarciar9005@gmail.com>
2025-01-24 14:16:31 -0500

    Add files via upload

---
+++ Procedimiento almacenado e Inserciones/Insert Branch Office.sql
@@ -0,0 +1,22 @@
+INSERT INTO [dbo].[BranchOffice]
+           ([company_id]
+           ,[broff_name]
+           ,[broff_code]
+           ,[broff_address]
+           ,[broff_city]
+           ,[broff_state]
+           ,[broff_country]
+           ,[broff_phone]
+           ,[broff_email]
+           ,[broff_active])
+     VALUES
+           (1, 'Hospital San José', 'HSJ001', 'Calle 10 Nº 18-75', 'Bogotá', 'Cundinamarca', 'Colombia', '1234567890', 'contacto@hsj.com', 1),
+           (1, 'Hospital Militar Central', 'HMC002', 'Transversal 3 Nº 49-02', 'Bogotá', 'Cundinamarca', 'Colombia', '0987654321', 'info@hmc.com', 1),
+           (1, 'Hospital Simón Bolívar', 'HSB003', 'Calle 165 # 7-06', 'Bogotá', 'Cundinamarca', 'Colombia', '1122334455', 'contacto@hsb.com', 1),
+           (1, 'Hospital Universitario San Ignacio', 'HUSI004', 'Cra. 7 #40-62', 'Bogotá', 'Cundinamarca', 'Colombia', '5566778899', 'info@husi.com', 1),
+           (1, 'Fundación Clínica Shaio', 'FCS005', 'Diagonal 115A Nº 70C-75', 'Bogotá', 'Cundinamarca', 'Colombia', '6677889900', 'contacto@shaio.com', 1),
+           (1, 'Hospital de La Samaritana', 'HLS006', 'Calle 45 # 16-41', 'Bogotá', 'Cundinamarca', 'Colombia', '2233445566', 'info@hls.com', 1),
+           (1, 'Hospital El Tunal', 'HET007', 'Cra. 24 # 48-51', 'Bogotá', 'Cundinamarca', 'Colombia', '3344556677', 'contacto@het.com', 1),
+           (1, 'Hospital de Engativá', 'HDE008', 'Calle 80 # 81-20', 'Bogotá', 'Cundinamarca', 'Colombia', '4455667788', 'info@hde.com', 1),
+           (1, 'Hospital de Suba', 'HDS009', 'Calle 132 # 91-20', 'Bogotá', 'Cundinamarca', 'Colombia', '5566778899', 'contacto@hds.com', 1),
+           (1, 'Hospital de Fontibón', 'HDF010', 'Calle 17 # 99-20', 'Bogotá', 'Cundinamarca', 'Colombia', '6677889900', 'info@hdf.com', 1);
